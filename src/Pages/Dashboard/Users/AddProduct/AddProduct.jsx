3b3952c06499c8600137711acb028b7cfd9df4e8
JannaturNur08 <nur@playense.com>
2023-11-26 03:56:57 +0600

    products json file added and also added in database

--- src/Pages/Dashboard/Users/AddProduct/AddProduct.jsx
+++ src/Pages/Dashboard/Users/AddProduct/AddProduct.jsx
@@ -1,6 +1,15 @@
+import { useForm } from "react-hook-form";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddProduct = () => {
+    const {register,handleSubmit,reset } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
+    
     return (
         <div>
             
