7f4faa8bd0d1c96152da37cf46d566329da7fc69
JannaturNur08 <nur@playense.com>
2023-11-25 14:53:41 +0600

    user deleted successfully in all users table

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
-
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 
@@ -13,6 +14,33 @@ const AllUsers = () => {
         return res.data;
     }
    })
+
+   const handleDeleteUser = user => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+
+            axiosSecure.delete(`/users/${user._id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        refetch();
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                        });
+                    }
+                })
+        }
+    });
+}
  
 
     return (
@@ -20,6 +48,56 @@ const AllUsers = () => {
             <div className="flex justify-evenly my-4">
                 <h2 className="text-3xl">All Users</h2>
                 <h2 className="text-3xl">Total Users: {users.length} </h2>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Make Moderator</th>
+                            <th>Make Admin</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    { user.role === 'moderator' ? 'Moderator' : <button
+                                        onClick={() => handleMakeModerator(user)}
+                                        className="btn btn-lg bg-orange-500">
+                                        <FaUsers className="text-white 
+                                        text-2xl"></FaUsers>Make Moderator
+                                    </button>}
+                                   
+                                </td>
+                                <td>
+                                    { user.role === 'admin' ? 'Admin' : <button
+                                        onClick={() => handleMakeAdmin(user)}
+                                        className="btn btn-lg bg-orange-500">
+                                        <FaUsers className="text-white 
+                                        text-2xl"></FaUsers>Make Admin
+                                    </button>}
+                                   
+                                </td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDeleteUser(user)}
+                                        className="btn btn-ghost btn-lg">
+                                        <FaTrashAlt className="text-red-600"></FaTrashAlt>
+                                    </button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
             </div>
        
         </div>
