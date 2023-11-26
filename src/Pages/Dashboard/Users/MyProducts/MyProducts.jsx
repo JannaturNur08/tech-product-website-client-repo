8fe4c1d5b951d768f8e16fe33a662166c390f6a4
JannaturNur08 <nur@playense.com>
2023-11-26 20:05:24 +0600

    product deleted successfully from myProducts page

--- src/Pages/Dashboard/Users/MyProducts/MyProducts.jsx
+++ src/Pages/Dashboard/Users/MyProducts/MyProducts.jsx
@@ -4,129 +4,121 @@ import useMyProducts from "../../../../hooks/useMyProducts";
 import { FaTrashAlt, FaUpload } from "react-icons/fa";
 
 const MyProducts = () => {
-    const [myProducts,refetch] = useMyProducts();
-    const axiosPublic = useAxiosPublic();
-    console.log(myProducts);
+	const [myProducts, refetch] = useMyProducts();
+	const axiosPublic = useAxiosPublic();
+	console.log(myProducts);
 
-    const handleUpdate = id => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
+	const handleUpdate = (id) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				axiosPublic.patch(`/products/${id}`).then((res) => {
+					if (res.data.modifiedCount > 0) {
+						refetch();
+						Swal.fire({
+							title: "Deleted!",
+							text: "Your product has been updated.",
+							icon: "success",
+						});
+					}
+				});
+			}
+		});
+	};
+	const handleDelete = (id) => {
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				axiosPublic.delete(`/products/${id}`).then((res) => {
+					if (res.data.deletedCount > 0) {
+						refetch();
+						Swal.fire({
+							title: "Deleted!",
+							text: "Your product has been deleted.",
+							icon: "success",
+						});
+					}
+				});
+			}
+		});
+	};
+	return (
+		<div>
+			<div>
+				<div className="overflow-x-auto">
+					<table className="table  w-full">
+						{/* head */}
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Product Name</th>
+								<th>Product Image</th>
+								<th>Number of Votes</th>
+								<th>Status</th>
+								<th>Update</th>
+								<th>Delete</th>
+							</tr>
+						</thead>
+						<tbody>
+							{myProducts.map((item, index) => (
+								<tr key={item._id}>
+									<th>{index + 1}</th>
+									<th>{item.product_name}</th>
 
-                axiosPublic.patch(`/products/${id}`)
-                    .then(res => {
-                        if (res.data.deletedCount > 0) {
-                            refetch();
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your file has been deleted.",
-                                icon: "success"
-                            });
-                        }
-                    })
-            }
-        });
-    }
-    const handleDelete = id => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-            if (result.isConfirmed) {
-
-                axiosPublic.delete(`/products/${id}`)
-                    .then(res => {
-                        if (res.data.deletedCount > 0) {
-                            refetch();
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your file has been deleted.",
-                                icon: "success"
-                            });
-                        }
-                    })
-            }
-        });
-    }
-    return (
-        <div>
-            <div>
-          
-            <div className="overflow-x-auto">
-                <table className="table  w-full">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>
-                                #
-                            </th>
-                            <th>Product Name</th>
-                            <th>Product Image</th>
-                            <th>Number of Votes</th>
-                            <th>Status</th>
-                            <th>Update</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            myProducts.map((item, index) => <tr key={item._id}>
-                                <th>
-                                    {index + 1}
-                                </th>
-                                <th>
-                                    {item.product_name}
-                                </th>
-
-                                <td>
-                                    <div className="flex items-center gap-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-12 h-12">
-                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    {item.vote}
-                                </td>
-                                <td>{item.status}</td>
-                                <th>
-                                    <button
-                                        onClick={() => handleUpdate(item._id)}
-                                        className="btn btn-ghost btn-lg">
-                                        <FaUpload className="text-red-600"></FaUpload>
-                                    </button>
-                                </th>
-                                <th>
-                                    <button
-                                        onClick={() => handleDelete(item._id)}
-                                        className="btn btn-ghost btn-lg">
-                                        <FaTrashAlt className="text-red-600"></FaTrashAlt>
-                                    </button>
-                                </th>
-                            </tr>)
-                        }
-
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        </div>
-    );
+									<td>
+										<div className="flex items-center gap-3">
+											<div className="avatar">
+												<div className="mask mask-squircle w-12 h-12">
+													<img
+														src={item.image}
+														alt="Avatar Tailwind CSS Component"
+													/>
+												</div>
+											</div>
+										</div>
+									</td>
+									<td>{item.vote}</td>
+									<td>{item.status}</td>
+									<th>
+										<button
+											onClick={() =>
+												handleUpdate(item._id)
+											}
+											className="btn btn-ghost btn-lg">
+											<FaUpload className="text-red-600"></FaUpload>
+										</button>
+									</th>
+									<th>
+										<button
+											onClick={() =>
+												handleDelete(item._id)
+											}
+											className="btn btn-ghost btn-lg">
+											<FaTrashAlt className="text-red-600"></FaTrashAlt>
+										</button>
+									</th>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MyProducts;
