8d098f0f07d9bd083224e4edf9b19eb96c0e53c5
JannaturNur08 <nur@playense.com>
2023-11-27 00:24:08 +0600

    status updated for reviewed products for moderator

--- src/Pages/Dashboard/Moderator/ReviewProducts/ReviewProducts.jsx
+++ src/Pages/Dashboard/Moderator/ReviewProducts/ReviewProducts.jsx
@@ -1,109 +1,147 @@
-
 import { Link } from "react-router-dom";
 import useProducts from "../../../../hooks/useProducts";
 
 import useAxiosPublic from "../../../../hooks/useAxiosPublic";
 import Swal from "sweetalert2";
 
-
 const ReviewProducts = () => {
-    const [products,refetch] = useProducts();
-    const axiosPublic = useAxiosPublic();
+	const [products, refetch] = useProducts();
+	const axiosPublic = useAxiosPublic();
 
-    const handleAccept = (productId) => {
-		axiosPublic.patch(`/products/${productId}`).then((res) => {
-			console.log(res.data);
-			if (res.data.modifiedCount > 0) {
-				refetch();
-				Swal.fire({
-					position: "top-end",
-					icon: "success",
-					title: `Product data is accepted!.`,
-					showConfirmButton: false,
-					timer: 1500,
-				});
-			}
-		});
+	const handleAccept = async (productId) => {
+		try {
+			await axiosPublic.patch(`api/products/${productId}`, {
+				status: "accepted",
+			});
+
+			// Update the state with the new status
+			const newProducts = products.map((product) =>
+				product._id === productId
+					? { ...product, status: "accepted" }
+					: product
+			);
+
+			refetch(newProducts);
+		} catch (error) {
+			console.error(`Error accepting product ${productId}:`, error);
+		}
 	};
-    return (
-        <div>
-            <div className="overflow-x-auto">
-					<table className="table  w-full">
-						{/* head */}
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>Product Name</th>
-								
-								<th>Details</th>
-								<th>Accept</th>
-								<th>Reject</th>
-								<th>Featured</th>
-							</tr>
-						</thead>
-						<tbody>
-							{products.map((item, index) => (
-								<tr key={item._id}>
-									<th>{index + 1}</th>
-									<th>{item.product_name}</th>
 
-								
-									
-									<th>
-                                    <Link to={`/productDetails/${item._id}`}>
-                                            <button
-                                                className="btn btn-ghost btn-md bg-blue-500 text-white">
-                                              View Details
-                                            </button>
-                                        </Link>
-									</th>
-									<th>
-                                  
-										{
-                                            item.status === "accepted" ? ("Accepted") : (
-                                                <button
+	const handleReject = async (productId) => {
+		try {
+			await axiosPublic.patch(`api/products/${productId}`, {
+				status: "rejected",
+			});
+
+			// Update the state with the new status
+			const newProducts = products.map((product) =>
+				product._id === productId
+					? { ...product, status: "rejected" }
+					: product
+			);
+
+			refetch(newProducts);
+		} catch (error) {
+			console.error(`Error rejecting product ${productId}:`, error);
+		}
+	};
+
+	const handleFeatured = async (productId) => {
+		try {
+			await axiosPublic.patch(`/api/products/${productId}`, {
+				Featured: true,
+			});
+
+			// Update the state with the new status
+			const newProducts = products.map((product) =>
+				product._id === productId
+					? { ...product, Featured: true }
+					: product
+			);
+
+			refetch(newProducts);
+		} catch (error) {
+			console.error(
+				`Error marking product ${productId} as featured:`,
+				error
+			);
+		}
+	};
+	return (
+		<div>
+			<div className="overflow-x-auto">
+				<table className="table  w-full">
+					{/* head */}
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Product Name</th>
+
+							<th>Details</th>
+							<th>Accept</th>
+							<th>Reject</th>
+							<th>Featured</th>
+						</tr>
+					</thead>
+					<tbody>
+						{products.map((item, index) => (
+							<tr key={item._id}>
+								<th>{index + 1}</th>
+								<th>{item.product_name}</th>
+
+								<th>
+									<Link to={`/productDetails/${item._id}`}>
+										<button className="btn btn-ghost btn-md bg-blue-500 text-white">
+											View Details
+										</button>
+									</Link>
+								</th>
+								<th>
+									{item.status === "accepted" ? (
+										"Accepted"
+									) : (
+										<button
 											onClick={() =>
 												handleAccept(item._id)
 											}
 											className="btn bg-green-500 btn-md text-white">
 											Accept
 										</button>
-                                            )
-                                        }
-									</th>
-									<th>
-                                    {
-                                            item.status === "rejected" ? ("Rejected") : (
-                                                <button
+									)}
+								</th>
+								<th>
+									{item.status === "rejected" ? (
+										"Rejected"
+									) : (
+										<button
 											onClick={() =>
 												handleReject(item._id)
 											}
 											className="btn bg-red-500 btn-md text-white">
 											Reject
 										</button>
-                                            )
-                                        }
-									</th>
-									<th>
-                                    {
-                                            item.Featured === true ? ("Featured") : (
-                                                <button
+									)}
+								</th>
+								<th>
+									{item.Featured === true ? (
+										"Featured"
+									) : (
+										<button
 											onClick={() =>
 												handleFeatured(item._id)
 											}
 											className="btn btn-outline btn-accent btn-md text-white">
 											Make Featured
 										</button>
-                                            )
-                                        }
-									</th>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				</div>
-        </div>
-    );
+									)}
+								</th>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default ReviewProducts;
